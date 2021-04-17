05b2f6503dbf2402e4e2f69be83cb99caac9b121
Pedro Henrique <alohapedrohenrique@gmail.com>
2021-04-17 12:47:56 -0400

    Adicionando arquivos ao repositorio Git

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,14 @@
+//JS é case sensitive.
+
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+
+const idade = 29;
+const nome = "Ricardo";
+
+//não fazer
+//mes = "Janeiro";
+
+
+
+
